20a022b2ca0d73b20fb7c86b60d67ae6b43e5ed0
zeusbf2 <zeusbf2@gmail.com>
2025-04-10 17:50:18 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,21 +1,16 @@
 import streamlit as st
+st.set_page_config(page_title="Visor ISV", layout="wide")
 import sys
 import os
-
-# Agrega el path del directorio actual (necesario en Streamlit Cloud)
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 from paginas.home import mostrar_home
 from paginas.isv_mejorado import mostrar_isv
 
-# Configuración de la app
-st.set_page_config(page_title="Visor ISV", layout="wide")
-
 # Menú lateral
 st.sidebar.title("Navegación")
 pagina = st.sidebar.selectbox("Selecciona una página", ["Home", "ISV Mejorado"])
 
-# Mostrar página correspondiente
 if pagina == "Home":
     mostrar_home()
 elif pagina == "ISV Mejorado":
