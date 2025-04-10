11c57301106ac848917ecfc8a9a5d18f18d340c5
zeusbf2 <zeusbf2@gmail.com>
2025-04-10 17:40:01 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,14 +1,15 @@
 import streamlit as st
-
-# === Menú lateral ===
-st.sidebar.title("Navegación")
-pagina = st.sidebar.selectbox("Selecciona una página", ["Home", "ISV Mejorado"])  # 👈 Home es el valor por defecto
-
-# === Importación de páginas ===
 from paginas.home import mostrar_home
 from paginas.isv_mejorado import mostrar_isv
 
-# === Cargar la página correspondiente ===
+# Configuración general
+st.set_page_config(page_title="Visor de Rutas ISV", layout="wide")
+
+# Menú lateral
+st.sidebar.title("Navegación")
+pagina = st.sidebar.selectbox("Selecciona una página", ["Home", "ISV Mejorado"])  # 👈 Home por defecto
+
+# Carga la página correspondiente
 if pagina == "Home":
     mostrar_home()
 elif pagina == "ISV Mejorado":
