6b56ec506ccbc7e075d9212cfd5e3674d66cbca4
zeusbf2 <zeusbf2@gmail.com>
2025-04-11 15:52:17 -0400

    Update ruta_3d.py

--- paginas/ruta_3d.py
+++ paginas/ruta_3d.py
@@ -3,7 +3,7 @@ if pagina == "3D":
     st.markdown("Abre el visor CesiumJS en una nueva pestaña:")
     st.markdown(
         """
-        <a href="https://tudominio.com/visor_3d_multiple/index.html" target="_blank">
+        <a href="https://infinityfy.com/visor_3d_multiple/index.html" target="_blank">
             <button style="padding:12px 20px;font-size:16px;background:#2d72d9;color:white;border:none;border-radius:6px;">
                 🌍 Abrir visor CesiumJS
             </button>
